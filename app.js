2e6ae44cba27b02357c93ea5aad79adc47fc2884
Melodystart <start99start@gmail.com>
2023-08-04 10:46:22 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const methodOverride = require('method-override') //可將方法由POST改為PUT
 const routes = require('./routes');
 require('./config/mongoose')
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs', helpers: multihelpers }))
 app.set('view engine', 'hbs')
@@ -19,6 +20,6 @@ app.use(bodyParser.urlencoded({ extended: true })); //才抓得到req
 app.use(methodOverride('_method')) //可將方法由POST改為PUT
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log('App is running on http://localhost:${PORT}')
 })
